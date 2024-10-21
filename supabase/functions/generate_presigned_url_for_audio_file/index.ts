09caefdacbe6b41a49750b2e21cbbed4e4abc409
Phong Nguyen <nhphong@Phongs-MacBook-Pro.local>
2024-10-21 12:48:28 +0700

    Remove the Supabase edge function

--- supabase/functions/generate_presigned_url_for_audio_file/index.ts
+++
@@ -1,9 +0,0 @@
-// index.ts
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-
-serve(async req => {
-    const { name } = (await req.json()) || { name: "World" };
-    return new Response(JSON.stringify({ message: `Hello, ${name}!` }), {
-        headers: { "Content-Type": "application/json" },
-    });
-});
