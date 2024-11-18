e479fd59db28858c73f0249f52e3c8202e501603
Phong Nguyen <phongnguyen@EA16.local>
2024-11-18 14:54:38 +0700

    Fixes VER-188: set the secret in fly.io and remove it from the script

--- scripts/start_prefect_server.sh
+++ scripts/start_prefect_server.sh
@@ -3,8 +3,5 @@
 # Create .prefect directory if it doesn't exist
 mkdir -p ~/.prefect
 
-# Convert postgres:// to postgresql+asyncpg://
-export PREFECT_API_DATABASE_CONNECTION_URL="${DATABASE_URL/postgres:\/\//postgresql+asyncpg:\/\/}"
-
 # Start the server
 prefect server start --host 0.0.0.0 --port 4200
