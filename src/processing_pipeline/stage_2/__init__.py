1100c4ce42977ef3a29886f01b01f9d0be01cf2b
Quan Cao <quan.cao@eastagile.com>
2026-02-23 15:55:25 +0700

    Refactor: Split stage 2 flows and tasks into separate modules

--- src/processing_pipeline/stage_2/__init__.py
+++ src/processing_pipeline/stage_2/__init__.py
@@ -6,12 +6,15 @@ from .tasks import (
     insert_new_snippet_to_snippets_table_in_supabase,
     ensure_correct_timestamps,
     process_llm_response,
-    audio_clipping,
     fetch_stage_1_llm_response_from_supabase,
     fetch_snippets_from_supabase,
     delete_snippet_from_r2,
     delete_snippet_from_supabase,
     reset_status_of_stage_1_llm_response,
+)
+
+from .flows import (
+    audio_clipping,
     undo_audio_clipping,
 )
 
